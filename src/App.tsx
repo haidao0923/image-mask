c62e9e6321559d2081f2c72adba8eab02e587845
hdao35 <hdao35@gatech.edu>
2024-01-22 21:16:26 -0500

    finish masking functionality

--- src/App.tsx
+++ src/App.tsx
@@ -7,78 +7,38 @@ interface ImageProps {
   maskSrc: string;
 }
 
-const Image = styled.img<ImageProps>`
-  mask-image: ${(p) => `url("${p.maskSrc}")`};
-  mask-repeat: no-repeat;
-  mask-size: contain;
-  width: 100%;
-  height: 100%;
-  object-fit: cover;
-`;
-
-const Grid = styled.div`
+const Grid = styled.div<ImageProps>`
   display: grid;
   grid-template-columns: repeat(2, 1fr);
   grid-template-rows: repeat(2, 1fr);
-  gap: 10px;
-`;
-
-const Quadrant = styled.div<{ position: string }>`
-  position: absolute;
-  width: 50%;
-  height: 50%;
-  overflow: hidden;
-
-  ${(props) => {
-    switch (props.position) {
-      case 'top-left':
-        return `
-          top: 0;
-          left: 0;
-        `;
-      case 'top-right':
-        return `
-          top: 0;
-          left: 50%;
-        `;
-      case 'bottom-left':
-        return `
-          top: 50%;
-          left: 0;
-        `;
-      case 'bottom-right':
-        return `
-          top: 50%;
-          left: 50%;
-        `;
-      default:
-        return '';
-    }
-  }}
-`;
-
-const Layout = styled.div`
-  padding: 2rem;
+  mask-image: ${(p) => `url("${p.maskSrc}")`};
+  mask-repeat: no-repeat;
+  mask-size: contain;
 `;
 
 function App() {
-  const image="https://images.unsplash.com/photo-1655043126269-d93dc7917a1e?crop=entropy&cs=tinysrgb&fm=jpg&ixid=MnwzMjM4NDZ8MHwxfHJhbmRvbXx8fHx8fHx8fDE2NTU5Mzc4ODU&ixlib=rb-1.2.1&q=80";
   const mask="https://static.vecteezy.com/system/resources/thumbnails/001/189/352/small/tree.png";
 
   return (
     <div className="App">
-      <header className="App-header">
+      <header className="background">
+        {/*The font is Bodidly*/}
+        <img className='title' src={require("./images/Logo2.png")}></img>
+        <h2 className='description'>Randomly generate art matching a themed silhouette</h2>
         <div className='image-container'>
-          <Grid>
-              <Quadrant position="top-left">
-                <Image src={require("./images/Nature/0.png")} maskSrc={mask} />
-              </Quadrant>
-              <Quadrant position="top-right">
-                <Image src={require("./images/Nature/0.png")} maskSrc={mask} />
-              </Quadrant>
+          <Grid maskSrc={mask}>
+                <img className='image' src={require("./images/Nature/0.png")}/>
+                <img className='image' src={require("./images/Nature/1.png")}/>
+                <img className='image' src={require("./images/Nature/2.png")}/>
+                <img className='image' src={require("./images/Nature/3.png")}/>
+          </Grid>
+          <Grid maskSrc={mask}>
+                <img className='image' src={require("./images/Nature/0.png")}/>
+                <img className='image' src={require("./images/Nature/1.png")}/>
+                <img className='image' src={require("./images/Nature/2.png")}/>
+                <img className='image' src={require("./images/Nature/3.png")}/>
           </Grid>
         </div>
-
       </header>
     </div>
   );

46bf5d3e7bba5c85a862dd779fba67c62a63ede9
Hai Dao <haixdao0923@gmail.com>
2024-01-24 19:26:09 -0500

    add scalability for 1-4 images per silhouette

--- src/App.tsx
+++ src/App.tsx
@@ -7,21 +7,33 @@ interface ImageProps {
   maskSrc: string;
 }
 
-const Grid = styled.div<ImageProps>`
-  display: grid;
-  grid-template-columns: repeat(2, 1fr);
-  grid-template-rows: repeat(2, 1fr);
-  mask-image: ${(p) => `url("${p.maskSrc}")`};
-  mask-position: center;
-  mask-repeat: no-repeat;
-  mask-size: contain;
-`;
-
 function App() {
   const mask="https://static.vecteezy.com/system/resources/thumbnails/001/189/352/small/tree.png";
   const [natureString, setNatureString] = useState<number[]>([0,0,0,0,0]);
   const [objectString, setObjectString] = useState<number[]>([0,0,0,0,0]);
   const [animalString, setAnimalString] = useState<number[]>([0,0,0,0,0]);
+  const [imageCount, setImageCount] = useState<number>(1);
+
+  const Grid = styled.div<ImageProps>`
+  display: grid;
+  grid-template-columns: repeat(${imageCount == 4 ? 2 : imageCount}, 1fr);
+  grid-template-rows: repeat(${imageCount == 4 ? 2 : 1}, 1fr);
+  height: min(30vw, 60vh);
+  mask-image: ${(p) => `url("${p.maskSrc}")`};
+  mask-position: center;
+  mask-repeat: no-repeat;
+  mask-size: 100% 100%;
+  `;
+
+  const StyledImage = styled.img`
+    height: min(${imageCount == 4 ? 15 : 30}vw, ${imageCount == 4 ? 30 : 60}vh);
+    width: min(${imageCount == 4 ? 15 : 30/imageCount}vw, ${imageCount == 4 ? 30 : 60/imageCount}vh);
+  `;
+
+  const handleDropdownChange = (event: React.ChangeEvent<HTMLSelectElement>) => {
+    const newValue = event.target.value;
+    setImageCount(parseInt(newValue));
+  };
 
   const randomizeImages = () => {
     const newNatureString: number[] = [Math.floor(Math.random() * 10),...Array.from({ length: 4 }, () => Math.floor(Math.random() * 14))];
@@ -43,25 +55,32 @@ function App() {
         {/*The font is Bodidly*/}
         <img className='title' src={require("./images/Logo2.png")}></img>
         <h2 className='description'>Randomly generate art matching a themed silhouette</h2>
-        <button onClick={randomizeImages}>Randomize</button>
+        <div className='choice-group'>
+            <div className='choice-text'>Image Count:</div>
+            <select id="myDropdown" value={imageCount} onChange={handleDropdownChange}>
+              <option value="1">1</option>
+              <option value="2">2</option>
+              <option value="3">3</option>
+              <option value="4">4</option>
+            </select>
+            <button onClick={randomizeImages}>Randomize</button>
+        </div>
+        <p></p>
         <div className='image-container'>
           <Grid maskSrc={require(`./masks/Nature/${natureString[0]}.png`)}>
-                <img className='image' src={require(`./images/Nature/${natureString[1]}.png`)}/>
-                <img className='image' src={require(`./images/Nature/${natureString[2]}.png`)}/>
-                <img className='image' src={require(`./images/Nature/${natureString[3]}.png`)}/>
-                <img className='image' src={require(`./images/Nature/${natureString[4]}.png`)}/>
+                {Array.from({length: imageCount}).map((_, index) => (
+                  <StyledImage src={require(`./images/Nature/${natureString[index + 1]}.png`)}/>
+                ))}
           </Grid>
           <Grid maskSrc={require(`./masks/Object/${objectString[0]}.png`)}>
-                <img className='image' src={require(`./images/Object/${objectString[1]}.png`)}/>
-                <img className='image' src={require(`./images/Object/${objectString[2]}.png`)}/>
-                <img className='image' src={require(`./images/Object/${objectString[3]}.png`)}/>
-                <img className='image' src={require(`./images/Object/${objectString[4]}.png`)}/>
+                {Array.from({length: imageCount}).map((_, index) => (
+                  <StyledImage src={require(`./images/Object/${objectString[index + 1]}.png`)}/>
+                ))}
           </Grid>
           <Grid maskSrc={require(`./masks/Animal/${animalString[0]}.png`)}>
-                <img className='image' src={require(`./images/Animal/${animalString[1]}.png`)}/>
-                <img className='image' src={require(`./images/Animal/${animalString[2]}.png`)}/>
-                <img className='image' src={require(`./images/Animal/${animalString[3]}.png`)}/>
-                <img className='image' src={require(`./images/Animal/${animalString[4]}.png`)}/>
+                {Array.from({length: imageCount}).map((_, index) => (
+                  <StyledImage src={require(`./images/Animal/${animalString[index + 1]}.png`)}/>
+                ))}
           </Grid>
         </div>
       </header>
